a104d30dafb08acd701cdf5bc76b7591bba6c35c
lukasstoecklein <lukas.stoecklein@gmail.com>
2017-09-20 12:47:54 +1200

    changing everything to be uploadable to EB, not working though (WSGI path error)

---
+++ application.py
@@ -0,0 +1,51 @@
+#!flask/bin/python
+from flask import Flask
+from flask import render_template
+application = Flask(__name__)
+
+@application.route('/')
+@application.route('/index')
+def index():
+    user = {'nickname': 'Miguel'}  # fake user
+    posts = [  # fake array of posts
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template("index.html",
+                           title='Home',
+                           user=user,
+                           posts=posts)
+
+@application.route('/our_campaign')
+def our_campaign():
+    return render_template("our_campaign.html",
+                           title='Our Campaign')
+
+@application.route('/who_we_are')
+def who_we_are():
+    return render_template("who_we_are.html",
+                           title='Who We Are')
+
+@application.route('/take_action')
+def take_action():
+    return render_template("take_action.html",
+                           title='Take Action')
+
+@application.route('/news')
+def news():
+    return render_template("news.html",
+                           title='News')
+
+@application.route('/available_support')
+def available_support():
+    return render_template("available_support.html",
+                           title='Available Support')
+
+if __name__ == "__main__":
+    application.run(debug = True)
